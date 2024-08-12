a1da8e28a0647623487b43fbec7127e37b94823c
Suetosha <honey.pancake2017@yandex.ru>
2024-08-12 16:23:03 +0200

    Updated draw_menu tag

--- django_app/template_tags/menu_tag.py
+++ django_app/template_tags/menu_tag.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 from django import template
 
 from django_app.models import Item
@@ -10,46 +8,64 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
-    active_menu_item = context['path'].split('/')[-1]
-    items = get_items_from_db(menu_name, active_menu_item)
-    items_tree = get_menu_tree(items, path='/menu/')
+
+    if 'path' in context and 'menu' in context and context['menu'] == menu_name:
+        active_menu_item = context['path'].split('/')[-1]
+        menu_name = context['menu']
+        items = get_items_from_db(menu_name, active_menu_item)
+    else:
+        items = Item.objects.filter(menu__name=menu_name, parent_id=None)
+
+    items = [{'id': i.id, 'name': i.name, 'menu_id': i.menu_id, 'parent_id': i.parent_id} for i in items]
+    items = list(reversed([i for i in items]))
+
+    items_tree = get_menu_tree(items, path=f'/{menu_name}/')
     rendered_items_tree = render_menu(items_tree)
     return rendered_items_tree
 
 
 def get_items_from_db(menu_name, active_menu_item):
     items = Item.objects.raw(
-        f'''
+        f'''        
             WITH RECURSIVE current AS (
                 SELECT initial.id, initial.name, initial.menu_id, initial.parent_id,  initial.parent_id AS level
                 FROM django_app_item as initial
-                    LEFT JOIN (SELECT id 
-                               FROM django_app_item WHERE parent_id=(SELECT id FROM django_app_item WHERE name="{active_menu_item}") LIMIT 1) as children
-                WHERE children.id is not null AND initial.id = children.id OR
-                  children.id is null AND initial.id = (SELECT id FROM django_app_item WHERE name="{active_menu_item}")
-    
+                    LEFT JOIN django_app_menu as menu ON initial.menu_id = menu.id
+                    LEFT JOIN (SELECT id
+                               FROM django_app_item
+                               WHERE parent_id=(SELECT id 
+                                                FROM django_app_item 
+                                                WHERE name="{active_menu_item}") LIMIT 1
+                                                )
+                                as children
+            
+                WHERE menu.name = "{menu_name}" AND
+                  (children.id is not null AND initial.id = children.id) OR
+                  (children.id is null AND initial.id = (SELECT id FROM django_app_item WHERE name="{active_menu_item}"))
+            
+            
                 UNION
                     SELECT sibling.id, sibling.name, sibling.menu_id, sibling.parent_id, current.level
                     FROM django_app_item sibling
+                        JOIN django_app_menu as menu ON sibling.menu_id = menu.id
                         JOIN current ON sibling.parent_id = current.parent_id
                         OR sibling.parent_id is null and current.parent_id is null
-    
+                    WHERE menu.name = "{menu_name}"
+            
                 UNION
                     SELECT parent.id, parent.name, parent.menu_id, parent.parent_id, current.level - 1
                     FROM django_app_item parent
+                        JOIN django_app_menu as menu ON parent.menu_id = menu.id
                         JOIN current ON current.parent_id = parent.id
-    
+                    WHERE menu.name = "{menu_name}"
                 )
             SELECT * FROM current;
         ''')
 
-    items = list(reversed([i for i in items]))
-
-    items = [{'id': i.id, 'name': i.name, 'menu_id': i.menu_id, 'parent_id': i.parent_id} for i in items]
     return items
 
 
-def get_menu_tree(items_list: list, menu=None, parent_id=None, path='/'):
+def get_menu_tree(items_list, menu=None, parent_id=None, path='/'):
     if menu is None:
         menu = []
 
--- django_app/urls.py
+++ django_app/urls.py
@@ -1,9 +1,10 @@
 from .views import MenuView
-from django.urls import re_path
+from django.urls import re_path, path
 
 app_name = 'app'
 
 urlpatterns = [
-    re_path(r'^menu/(?P<path>[a-zA-Z\/ ]*)/$', MenuView.as_view(), name='menu'),
+    path('', MenuView.as_view(), name='menu'),
+    re_path(r'^(?P<menu>[^/]+)/(?P<path>[a-zA-Z\/ 0-9]*)?/$', MenuView.as_view(), name='menu'),
 
 ]
